1efba24cc8224637ec88610e86d9ae1f5779870f
Majai <majairoidu@gmail.com>
2021-06-14 18:17:31 +0530

    Signin page added, Cleared warnings from ProductAction page

--- src/App.js
+++ src/App.js
@@ -8,38 +8,90 @@ import {Provider} from 'react-redux';
 import store from './store.js';
 import Left from "./components/Left";
 import Nav from "./components/Nav";
+import {connect} from "react-redux";
 import Dashboard from "./components/Dashboard";
 import PlantManager from "./components/PlantManager/Index.js";
 import ProductManager from "./components/ProductManager/Index.js";
 import {createBrowserHistory} from 'history';
+import {authEmailPassword,checkLogin} from "./actions/authAction";
+import SignIn from './components/SignIn/index'
+import Sidebar from "./components/Sidebar";
 export const history = createBrowserHistory({forceRefresh: true})
 
-function App() {
+function App(props) {
+  
+  const authKey = props.authKey;
+  
+  console.log("authKey", authKey)
+  console.log("loggedIn", authKey.loggedIn)
   return (
+    <div>
     
     <Router>
-      <Provider store={store}>
+    <Provider store={store}>
+    {/* {authKey.loggedIn ? <> */}
       <div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
-        <Nav />
-        <Left />
+       
           <div id="content" class="content">
-            <Switch>
-              <Route path="/" exact>
-                <Dashboard />
-              </Route>
-              <Route path="/plantManager">
-                <PlantManager/>
-              </Route>
-              <Route path="/productManager">
-                <ProductManager/>
-              </Route>
-            </Switch>
+                <Switch>
+
+                <Route path="/" exact>
+                    <SignIn />
+                  </Route>
+            
+
+                  <Route path="/Dashboard" exact>
+                  <Nav />
+                  <Left />
+                    <Dashboard />
+                  </Route>
+                  <Route path="/plantManager">
+                  <Nav />
+                  <Left />
+                    <PlantManager/>
+                  </Route>
+                  <Route path="/productManager">
+                  <Nav />
+                  <Left />
+                    <ProductManager/>
+                  </Route>
+
+                </Switch>
+               
           </div>
       </div>
 
+      {/* </>:<SignIn/>} */}
       </Provider>
     </Router>
+   
+
+    </div>
   );
 }
 
-export default App;
+
+
+const mapStateToProps = (state)=> ({
+  authKey:state.authKey,
+  
+
+
+})
+export default connect(mapStateToProps,{checkLogin})(App)
+
+//export default App;
+
+
+
+// export const RootApp= ()=>{
+//   return(
+//     <div>
+//           <Provider store={store}>
+//               <Router>
+//                 <App />
+//               </Router>
+//         </Provider>
+//     </div>
+//   )
+// }
