e6d00a91a994908d2ff9f07b82397e520cebbfd8
varshitha-zvky <varshitha@zvky.com>
2021-06-23 10:05:22 +0530

    grid fix by karthik

--- src/components/PlantManager/SkuList.js
+++ src/components/PlantManager/SkuList.js
@@ -1,3 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+/* eslint-disable eqeqeq */
+/* eslint-disable react/jsx-no-duplicate-props */
 import React, { useState,useEffect } from 'react'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
@@ -31,6 +35,8 @@ const SkuList = (props)=>{
     const [message,setMessage] = useState("")
     const [type, setType] = useState("")
     const [pageSize, setPageSize] =useState(15)
+    const [errorObj,setErrorObj] = useState({ each_cost:0,each_price:0,sale_price:0  })
+    const [errorCount,setErrorCount] = useState(0)
     const handleChange=(date)=> {
         setStartDate(date)
       }
@@ -53,11 +59,55 @@ const SkuList = (props)=>{
 
     }
     const handleInput =(e)=>{
+        let errorcount =errorCount
+        let errorobj =errorObj
+        if(e.target.id  === "each_cost" ){
+            errorobj.each_cost=0
+            errorcount--
+        }
+        if(e.target.id  === "each_price" ){
+            errorobj.each_price=0
+            errorcount--
+        }
+        if(e.target.id  === "sale_price" ){
+            errorobj.sale_price=0
+            errorcount--
+        }
+        setErrorObj(errorobj)
+       setErrorCount(errorcount)
         if(e.target.id =="archived") props.handlePlantSkuInputAction(e.target.id,e.target.value ==1?0:1)
         else if(e.target.id =="status") props.handlePlantSkuInputAction(e.target.id,e.target.value ==1?0:1)
         else props.handlePlantSkuInputAction(e.target.id,e.target.value)
 
     }
+    const handleValidation = () => {
+//         let returnValue=true
+//         let errorcount =errorCount
+//         let errorobj =errorObj
+//       if(plantDataById.genus.length === 0){
+//        returnValue= false
+//        errorobj.genus=1
+//        errorcount++
+//       }
+//       if(plantDataById.species.length === 0){
+//        returnValue= false
+//        errorobj.species=1
+//        errorcount++
+//       }
+//    //    if(plantDataById.categoryData.length === 0){
+//    //     returnValue= false
+//    //     errorobj.categoryData=1
+//    //     errorcount++
+//    //    }
+//       setErrorObj(errorobj)
+//       setErrorCount(errorcount)
+
+   }
+   const handleSubmit = ()=>{
+    // let validate = handleValidation()  
+    // if(validate)
+    // props.updateSkuAction(skuDataById.product_id,skuDataById,skuValidation)
+   }
     const [id,setId] = useState(0)
     const [open,setOpen] = useState(false)
 
@@ -96,6 +146,7 @@ const SkuList = (props)=>{
    console.log(props.plantData);
       const {plantData,plantSkuData,plantSkuPageNumber,needAction,plantSkuDataById} = props.plantData
       const plantPerPage = pageSize;
+      const totalLength = plantSkuData.length;
       const pagesVisited = plantSkuPageNumber*pageSize;
       const displayPlantSkuList = plantSkuData.slice(pagesVisited,pagesVisited+plantPerPage)
       const pageCount = Math.ceil(plantSkuData.length/plantPerPage)
@@ -169,7 +220,7 @@ const SkuList = (props)=>{
                                             <label>Packaging <span class="text-danger">*</span></label>
                                             <select class="form-control" id="packaging" id={allAttributes.length>0?allAttributes.filter(formData=>formData.name =="Packaging")[0]["id"]:"packaging"} onChange={handleInput}>
                                             <option>None</option>
-                                            {allAttributes.length>0?allAttributes.filter(formData=>formData.name =="Packaging").map(filterData=>{
+                                            {allAttributes.length>0?allAttributes.filter(formData=>formData.name ==="Packaging").map(filterData=>{
                                                     return (filterData.sub_attributes.map(subData=>{
                                                     
                                                         return(<option value={subData.id} selected={plantSkuDataById.attributes_subattributes.filter(filter=>filter.subattribute_id == subData.id).length==0?"selected":""}>{subData.value}</option>)
@@ -245,7 +296,7 @@ const SkuList = (props)=>{
                             </div>
                                <div className="row_1">
                             <div>
-                            <label className="greenText">{"Showing " + (( plantSkuPageNumber)+1 )+  "  to  " +  ((( pageSize+1)*5)>plantSkuData.length?plantSkuData.length:( plantSkuPageNumber+1)*5)  + "  of   "  +   plantSkuData.length }</label>
+                            <label className="greenText">{"Showing " + (plantSkuPageNumber>0 ? (pageSize*((plantSkuPageNumber)))+1 : ((plantSkuPageNumber)+1))+  "  to  " +  (plantSkuPageNumber>0 ? (((pageSize*((plantSkuPageNumber)))+pageSize)>totalLength ? totalLength : ((pageSize*((plantSkuPageNumber)))+pageSize)) : ((((plantSkuPageNumber)+1)*pageSize)>totalLength?totalLength:(((plantSkuPageNumber)+1)*pageSize)))   + "  of   "  +   totalLength }</label>
                             </div>
 
 
@@ -269,67 +320,69 @@ const SkuList = (props)=>{
 
 
 
-                    <div className="skuPagination" style={{marginRight:"26em"}}>
+                    <div className="skuPagination">
                     <TablePagination pageChange={paginationChange} pageCount={pageCount} pageNumber={plantSkuPageNumber+1}/>
                     </div>
                     </div>
-                            <div class="bg-white px-3 py-3 my-3">
-                                <table id="plantDetails" class="table table-striped w-100 table-responsive">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-nowrap">Status</th>
-                                            <th class="text-nowrap">SKU</th>
-                                            <th class="text-nowrap">Each Cost</th>
-                                            <th class="text-nowrap">Each Price</th>
-                                            <th class="text-nowrap">Sale Price</th>
-                                            <th class="text-nowrap">Sale Active</th>
-                                            <th class="text-nowrap">Volume Per Unit</th>
-                                            <th class="text-nowrap">Volume QTY</th>
-                                            <th class="text-nowrap text-center">Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                            <div className="form-group row mt-3">
+                                <div className="col-md-12">
+                                    <table id="plantDetails" class="table table-striped w-100">
+                                        <thead>
+                                            <tr>
+                                                <th class="text-nowrap">Status</th>
+                                                <th class="text-nowrap">SKU</th>
+                                                <th class="text-nowrap">Each Cost</th>
+                                                <th class="text-nowrap">Each Price</th>
+                                                <th class="text-nowrap">Sale Price</th>
+                                                <th class="text-nowrap">Sale Active</th>
+                                                <th class="text-nowrap">Volume Per Unit</th>
+                                                <th class="text-nowrap">Volume QTY</th>
+                                                <th class="text-nowrap text-center">Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
 
-                                    {
-                    displayPlantSkuList.map(skuData=>{
-                        return(
-                                        <tr>
-                                            <td>{skuData.archived ===0?"Active":"Inactive"}</td>
-                                            <td>{skuData.sku_code}</td>
-                                            <td>{skuData.each_cost}</td>
-                                            <td>{skuData.each_price}</td>
-                                            <td>{skuData.sale_price}</td>
-                                            <td class="text-center">
-                                                <div class="custom-control custom-checkbox mb-1">
-                                                    <input type="checkbox" class="custom-control-input"checked={skuData.status==0?false:true}/>
-                                                    <label class="custom-control-label" for="customCheck1"></label>
-                                                </div>
-                                            </td>
-                                            <td>{skuData.volume_price_per_unit}</td>
-                                            <td>{skuData.volume_quantity}</td>
-                                            <td class="text-center">
-                                                <span>
-                                                    {/* <a href="javascript:;"> */}
-                                                        <img src="assets/img/edit.svg" alt="" onClick={()=>getSpecifiedplant(skuData.plant_id,"edit","sku")}/>
-                                                    {/* </a> */}
-                                                </span>
-                                                <span>
-                                                    {/* <a href="javascript:;"> */}
-                                                        <img src="assets/img/duplicate.svg" alt=""/>
-                                                    {/* </a> */}
-                                                </span>
-                                                <span>
-                                                    {/* <a href="javascript:;"> */}
-                                                        <img src="assets/img/delete.svg" alt="" onClick={()=>confirmAction(skuData.plant_id)}/>
-                                                    {/* </a> */}
-                                                </span>
-                                            </td>
-                                        </tr>                                       
-                                       )
-                                    })
-                                  }
-                                    </tbody>
-                                </table>
+                                        {
+                        displayPlantSkuList.map(skuData=>{
+                            return(
+                                            <tr>
+                                                <td>{skuData.archived ===0?"Active":"Inactive"}</td>
+                                                <td>{skuData.sku_code}</td>
+                                                <td>{skuData.each_cost}</td>
+                                                <td>{skuData.each_price}</td>
+                                                <td>{skuData.sale_price}</td>
+                                                <td class="text-center">
+                                                    <div class="custom-control custom-checkbox mb-1">
+                                                        <input type="checkbox" class="custom-control-input"checked={skuData.status==0?false:true}/>
+                                                        <label class="custom-control-label" for="customCheck1"></label>
+                                                    </div>
+                                                </td>
+                                                <td>{skuData.volume_price_per_unit}</td>
+                                                <td>{skuData.volume_quantity}</td>
+                                                <td class="text-center">
+                                                    <span>
+                                                        {/* <a href="javascript:;"> */}
+                                                            <img src="assets/img/edit.svg" alt="" onClick={()=>getSpecifiedplant(skuData.plant_id,"edit","sku")}/>
+                                                        {/* </a> */}
+                                                    </span>
+                                                    <span>
+                                                        {/* <a href="javascript:;"> */}
+                                                            <img src="assets/img/duplicate.svg" alt=""/>
+                                                        {/* </a> */}
+                                                    </span>
+                                                    <span>
+                                                        {/* <a href="javascript:;"> */}
+                                                            <img src="assets/img/delete.svg" alt="" onClick={()=>confirmAction(skuData.plant_id)}/>
+                                                        {/* </a> */}
+                                                    </span>
+                                                </td>
+                                            </tr>                                       
+                                        )
+                                        })
+                                    }
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
         </div>
