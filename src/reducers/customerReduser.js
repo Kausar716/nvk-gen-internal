bad904e53dee50d3f4767979545e13cf420ab583
DESKTOP-PEORPMR\kausar <kausar@zvky.com>
2021-07-12 19:46:47 +0530

    minior filter issue

--- src/reducers/customerReduser.js
+++ src/reducers/customerReduser.js
@@ -67,9 +67,7 @@ const initialSatate = {
              
                 else if(action.searchData !=="" && state.alphabetSearch !=="All")
                 // alert("hhh")
-                    searchedData = datatoShow.filter(filterData=>(filterData.name.toLowerCase().includes(action.searchData.toLowerCase())) &&
-                    filterData.name.toLowerCase().includes(state.alphabetSearch.toLowerCase())
-                    )
+                searchedData = datatoShow.filter(filterData=>(filterData.name.toLowerCase().charAt(0)===state.alphabetSearch.toLowerCase().charAt(0)) &&(filterData.name.toLowerCase().charAt(0)===action.searchData.toLowerCase().charAt(0)))
                 else if(action.searchData ==="" && state.alphabetSearch ==="All"){
                     searchedData =datatoShow
 
