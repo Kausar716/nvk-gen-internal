9230d72c56affb621082d26bc61b6c637d5182de
varshitha-zvky <varshitha@zvky.com>
2021-08-18 15:35:07 +0530

    NVK-142 in plant

--- src/components/PlantManager/SkuList.js
+++ src/components/PlantManager/SkuList.js
@@ -583,7 +583,7 @@ const SkuList = (props)=>{
                                                 </td>
 
                                                 <td class="text-right" style={{color:skuData.volume_price_per_unit==="0.00"?"lightgray":""}} >{skuData.volume_price_per_unit===null?"0.00":skuData.volume_price_per_unit}</td>
-                                                <td class="text-right" style={{color:skuData.volume_price_per_unit==="0.00"?"lightgray":""}} >{skuData.volume_quantity_name}</td>
+                                                <td class="text-right" style={{color:skuData.volume_price_per_unit==="0.00"?"lightgray":""}} >{skuData.volume_price_per_unit===null || skuData.volume_price_per_unit === "0.00"?"None":skuData.volume_quantity_name}</td>
                                                 <td class="text-center" >
                                                     <span>
                                                         {/* <a href="javascript:;"> */}
