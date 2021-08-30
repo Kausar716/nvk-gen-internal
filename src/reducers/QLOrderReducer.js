f9d932046f89e6be3b84db238289df57698c4856
Majai <majairoidu@gmail.com>
2021-08-30 22:16:21 +0530

    QuoteList -Api integration and filtering for table grid. -Checkbox selection display -Search filter suggestion for other remaining fields -Jira Bugs -ZVKYNVK-177 -ZVKYNVK-170 --NVK-183

--- src/reducers/QLOrderReducer.js
+++ src/reducers/QLOrderReducer.js
@@ -7,6 +7,7 @@ import {
     // FILTER_DATA_BY_SEARCH,
     // FILTER_DATA_BY_ALPHA,
     HANDLE_INPUT,
+    GET_QUOTE_ORDER_LIST
    
 
     
@@ -18,33 +19,35 @@ import {
    
 
 const initialSatate = {
-   purchaseOrderList:[
-    {status:"closed", poNumber:"JSMITH-012301-1", suppliearName:"John Smith landscaping", 
-    supplierOrder:"1024275", createdBy:"John Smith", orderDate:"20/05/2021", expectedDate:"20/05/12021",
-     dispatch:"Pickup", amount:"6,085.00"},
+//    purchaseOrderList:[
+//     {status:"closed", poNumber:"JSMITH-012301-1", suppliearName:"John Smith landscaping", 
+//     supplierOrder:"1024275", createdBy:"John Smith", orderDate:"20/05/2021", expectedDate:"20/05/12021",
+//      dispatch:"Pickup", amount:"6,085.00"},
 
-     {status:"closed", poNumber:"WILLSMITH-012301-1", suppliearName:"WILL Smith landscaping", 
-     supplierOrder:"2024275", createdBy:"Will Smith", orderDate:"20/06/2021", expectedDate:"20/08/2021",
-      dispatch:"Pickup", amount:"6,085.00" },
+//      {status:"closed", poNumber:"WILLSMITH-012301-1", suppliearName:"WILL Smith landscaping", 
+//      supplierOrder:"2024275", createdBy:"Will Smith", orderDate:"20/06/2021", expectedDate:"20/08/2021",
+//       dispatch:"Pickup", amount:"6,085.00" },
 
-      {status:"open", poNumber:"Scena-012301-1", suppliearName:"John Scena landscaping", 
-      supplierOrder:"1024275", createdBy:"John Scena", orderDate:"20/05/12021", expectedDate:"20/05/12021",
-       dispatch:"Delivery", amount:"6,085.00" },
+//       {status:"open", poNumber:"Scena-012301-1", suppliearName:"John Scena landscaping", 
+//       supplierOrder:"1024275", createdBy:"John Scena", orderDate:"20/05/12021", expectedDate:"20/05/12021",
+//        dispatch:"Delivery", amount:"6,085.00" },
 
-       {status:"Draft", poNumber:"Jason-012301-1", suppliearName:"Jason Smith landscaping", 
-       supplierOrder:"24275", createdBy:"Jason Smith", orderDate:"20/05/2021", expectedDate:"20/09/2021",
-        dispatch:"Pickup", amount:"6,085.00" },
+//        {status:"Draft", poNumber:"Jason-012301-1", suppliearName:"Jason Smith landscaping", 
+//        supplierOrder:"24275", createdBy:"Jason Smith", orderDate:"20/05/2021", expectedDate:"20/09/2021",
+//         dispatch:"Pickup", amount:"6,085.00" },
 
-        {status:"closed", poNumber:"Dweny-012301-1", suppliearName:"Dweny Smith landscaping", 
-        supplierOrder:"1024275", createdBy:"Dweny Smith", orderDate:"20/02/12021", expectedDate:"20/05/12021",
-         dispatch:"Pickup", amount:"6,085.00" },
+//         {status:"closed", poNumber:"Dweny-012301-1", suppliearName:"Dweny Smith landscaping", 
+//         supplierOrder:"1024275", createdBy:"Dweny Smith", orderDate:"20/02/12021", expectedDate:"20/05/12021",
+//          dispatch:"Pickup", amount:"6,085.00" },
 
-         {status:"closed", poNumber:"Robert Jr-012301-1", suppliearName:"Robert Jr Smith landscaping", 
-         supplierOrder:"1024275", createdBy:"Robert Jr Smith", orderDate:"20/05/12021", expectedDate:"20/05/12021",
-          dispatch:"Delivery", amount:"6,085.00" }
-   ],
+//          {status:"closed", poNumber:"Robert Jr-012301-1", suppliearName:"Robert Jr Smith landscaping", 
+//          supplierOrder:"1024275", createdBy:"Robert Jr Smith", orderDate:"20/05/12021", expectedDate:"20/05/12021",
+//           dispatch:"Delivery", amount:"6,085.00" }
+//    ],
    pageNumber:0,
    duplicateData:[],
+   quoteOrderList:[],
+   backupQOL:[],
    //activeData:[],
    //inactiveData:[],
    //radioFilter:"active",
@@ -65,6 +68,14 @@ const initialSatate = {
                 [action.id]:action.data
 
             }
+
+
+            // case GET_QUOTE_ORDER_LIST:
+            //     return{
+            //         ...state,
+            //         quoteOrderList:action.payload,
+            //         backupQOL:action.payload              
+            //     }
      
             case SET_PAGE_NUMBER_PO:
                 return{
